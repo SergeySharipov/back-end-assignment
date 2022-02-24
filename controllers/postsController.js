e8c36747333558305ab462ec55c885718c307762
SergeySharipov <sharipov14@gmail.com>
2022-02-24 12:24:36 -0500

    Handle tags required error

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,6 +2,11 @@ const postsController = {
   getPosts: async (req, res) => {
     const { tags, sortBy, direction } = req.query
 
+    if (!tags) {
+      res.status(400).json({ error: 'Tags parameter is required' })
+      return
+    }
+
     res.status(200).json({ tags, sortBy, direction })
   }
 }
