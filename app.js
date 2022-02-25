57037156fbe278fa823e04dbf53147497e27d1a0
SergeySharipov <sharipov14@gmail.com>
2022-02-25 01:39:12 -0500

    Add request logger for development

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
+const middleware = require('./utils/middleware')
+
+app.use(middleware.requestLogger)
 
 app.use('/', routes)
 
--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,13 @@
 const info = (...params) => {
-  console.log(...params)
+  if (process.env.NODE_ENV === 'development') {
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
-  console.error(...params)
+  if (process.env.NODE_ENV === 'development') {
+    console.error(...params)
+  }
 }
 
 module.exports = {
